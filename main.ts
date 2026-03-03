810451acb516f54d10d19703cec3a9392fc42988
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-03 13:24:46 -0500

    update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Feb 2026
+ * This program does a cookie clicker game
 */
 
-basic.showString('Hello, World!')
+// setup
+let cookies: number = 0
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// adding cookie points
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
